1fde1e44bc29044772e7c45fe2e291573f9c013d
Ecophagy <olly.spoon@gmail.com>
2021-01-27 11:24:47 +0000

    Added empty files for player lists 2016-2020

--- Generatefiles.py
+++ Generatefiles.py
@@ -1,3 +1,12 @@
-for i in range (2010, 2016):
-    for q in range (1,5):
-        open("Input/MafiaPlayerList" + str(i) + "Q" + str(q) + ".txt", 'a').close()
+import os
+
+
+def generate_empty_text_input_files():
+    for i in range(2010, 2021):
+        for q in range(1, 5):
+            path = os.path.join("Input", "MafiaPlayerList" + str(i) + "Q" + str(q) + ".txt")
+            open(path, 'a').close()
+
+
+if __name__ == "__main__":
+    generate_empty_text_input_files()
